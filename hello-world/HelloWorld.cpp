14d15d2ce81bfa3ffbc27df85bf9b5a7e89e8dec
Vince Tran <vietanhtran.96@gmail.com>
2022-05-03 22:04:50 -0500

    Initial commit

---
+++ hello-world/HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    std::cout << "Hello, world";
+
+    // Add these lines to wait for user to press enter
+    //  before exiting
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+
+    return 0;
+}
---
+++ hello-world/README.md
@@ -0,0 +1,9 @@
+# To compile
+```
+g++ -o HelloWorld HelloWorld.cpp
+```
+
+# To run
+```
+./HelloWorld
+```
